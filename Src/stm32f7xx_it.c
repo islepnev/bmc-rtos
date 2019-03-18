debd4062832be9753860aafed817e72f87a8c82a
Ilja Slepnev <islepnev@jinr.ru>
2019-03-18 12:38:40 +0300

    handle uart3 irq

--- Src/stm32f7xx_it.c
+++ Src/stm32f7xx_it.c
@@ -96,6 +96,7 @@ void HardFault_Handler(void)
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
+extern UART_HandleTypeDef huart3;
 
 extern TIM_HandleTypeDef htim1;
 
@@ -216,6 +217,19 @@ void TIM1_UP_TIM10_IRQHandler(void)
   /* USER CODE BEGIN TIM1_UP_TIM10_IRQn 1 */
 
   /* USER CODE END TIM1_UP_TIM10_IRQn 1 */
+}
+
+/**
+* @brief This function handles USART3 global interrupt.
+*/
+void USART3_IRQHandler(void)
+{
+  /* USER CODE BEGIN USART3_IRQn 0 */
+  /* USER CODE END USART3_IRQn 0 */
+  HAL_UART_IRQHandler(&huart3);
+  /* USER CODE BEGIN USART3_IRQn 1 */
+
+  /* USER CODE END USART3_IRQn 1 */
 }
 
 /* USER CODE BEGIN 1 */
