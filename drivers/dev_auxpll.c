86882a0c45f121ee51bd01a52c955d8c5e014190
Andrey Shchipunov <andrey.shchipunov@gmail.com>
2019-11-12 09:09:49 +0300

    Added OUT7, OUT8 and OUT9 configurations for PLL AD9516-4 (regs @0x0141~0x0143)

--- drivers/dev_auxpll.c
+++ drivers/dev_auxpll.c
@@ -455,7 +455,10 @@ OpStatusTypeDef auxpllSetup(Dev_auxpll *d)
     ad9516_write1(AD9516_REG1_CLOCKS, reg_clocks.raw);
 
     // output drivers
-    ad9516_write1(0x140, 0x42);
+    ad9516_write1(0x140, 0x42); // OUT6, Enable, LVDS
+    ad9516_write1(0x141, 0x43); // OUT7, Disable,
+    ad9516_write1(0x142, 0x42); // OUT8, Enable, LVDS
+    ad9516_write1(0x143, 0x42); // OUT9, Enable, LVDS
 
     // output dividers
     ad9516_write1(0x199, 0);
