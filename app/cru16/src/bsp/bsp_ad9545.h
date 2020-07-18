200fa2cd32b3d2fe9fdfcd1c34ef3e6c4dbf633a
Ilja Slepnev <islepnev@gmail.com>
2020-07-19 00:22:02 +0300

    added pll_ref_div for cru16

--- app/cru16/src/bsp/bsp_ad9545.h
+++ app/cru16/src/bsp/bsp_ad9545.h
@@ -22,7 +22,7 @@
 #define PLL_REF_PERIOD_NS PLL_REF_PERIOD_NS_CRU16
 
 #define PLL_REF_DIV_TTVXS 209
-#define PLL_REF_DIV_CRU16 (209*3)
+#define PLL_REF_DIV_CRU16 625
 #define PLL_REF_DIV PLL_REF_DIV_CRU16
 
 #include "ad9545/ad9545_setup_regs.h"
