00cc24b5d998380f8a4ae98bfa46b6f06ba43764
Ilja Slepnev <islepnev@jinr.ru>
2020-07-22 13:10:34 +0300

    del unused files

--- cru16/cubemx/clean_sources.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-find Core Drivers LWIP Middlewares TrueSTUDIO \
--iname '*.h' -o -iname '*.c' -o -iname '*.s' | while read f; do
-    dos2unix $f
-    sed -i -e 's/[ \t]*$//' $f
-done
