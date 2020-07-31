f67fcbc20e68e63449f8552e4dd4e355eed74e5d
Ilja Slepnev <islepnev@jinr.ru>
2020-07-31 17:20:27 +0300

    fix text

--- drivers/dev_common_types.c
+++ drivers/dev_common_types.c
@@ -33,9 +33,9 @@ const char *sensor_status_ansi_str(SensorStatus state)
 const char *sensor_status_text(SensorStatus state)
 {
     switch(state) {
-    case SENSOR_UNKNOWN:  return " UNKNOWN";
-    case SENSOR_NORMAL:   return "  NORMAL";
-    case SENSOR_WARNING:  return " WARNING";
+    case SENSOR_UNKNOWN:  return "UNKNOWN";
+    case SENSOR_NORMAL:   return "NORMAL";
+    case SENSOR_WARNING:  return "WARNING";
     case SENSOR_CRITICAL: return "CRITICAL";
     default: return "FAIL";
     }
