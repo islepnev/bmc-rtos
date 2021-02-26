cf962020ee4177d9f447c2b63a445ea0572ea8b8
Ilia Slepnev <islepnev@gmail.com>
2021-02-26 13:45:16 +0300

    pll ad9545: add TQDC board config

--- drivers/ad9545/board_config_ad9545.h
+++ drivers/ad9545/board_config_ad9545.h
@@ -66,6 +66,27 @@
 #define PLL_DIST_DIV_1_A     13
 #define PLL_DIST_DIV_1_B     13
 
+#elif defined (BOARD_TQDC)
+
+#define DPLL0_BW_HZ          500
+#define DPLL1_BW_HZ          500
+#define PLL_REFA_PERIOD_NS   24
+#define PLL_REFB_PERIOD_NS   24
+#define PLL_REFA_DIV         209
+#define PLL_REFB_DIV         209
+#define DPLL0_EXT_ZD         1
+#define DPLL0_ZD_FB_Path     PROFILE_EXT_ZD_FEEDBACK_REFB
+#define APLL0_Frequency      2.5e9
+#define APLL0_M_Divider      8
+#define PLL_DIST_DIV_0_A     30
+#define PLL_DIST_DIV_0_B     10
+#define PLL_DIST_DIV_0_C     30
+#define DPLL1_EXT_ZD         0
+#define DPLL1_ZD_FB_Path     PROFILE_INT_ZD_FEEDBACK_OUT1A
+#define APLL1_Frequency      3.25e9
+#define APLL1_M_Divider      10
+#define PLL_DIST_DIV_1_A     39
+#define PLL_DIST_DIV_1_B     13
 #else
 
 #define DPLL0_BW_HZ          500
