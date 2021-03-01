fc432385b3c20d3c5c129d831929bc4a823e4e07
Ilia Slepnev <islepnev@gmail.com>
2021-03-01 23:07:16 +0300

    snmp_set_device_enterprise_oid

--- src/tasks/snmp/snmp_agent.c
+++ src/tasks/snmp/snmp_agent.c
@@ -53,6 +53,23 @@ static const struct snmp_mib *mibs[] = {
 };
 #endif /* LWIP_SNMP */
 
+#if defined(BOARD_CRU16)
+#define BOARD_OID 1
+#elif defined(BOARD_TTVXS)
+#define BOARD_OID 2
+#elif defined(BOARD_TQDC)
+#define BOARD_OID 3
+#else
+#error Unknown BOARD
+#endif
+
+#define MY_DEVICE_ENTERPRISE_OID {1, 3, 6, 1, 4, 1, 53776, 120, 2, 1, BOARD_OID}
+#define MY_DEVICE_ENTERPRISE_OID_LEN 11
+
+static const struct snmp_obj_id  device_enterprise_oid = {
+    MY_DEVICE_ENTERPRISE_OID_LEN, MY_DEVICE_ENTERPRISE_OID
+};
+
 void
 snmp_agent_init(void)
 {
@@ -71,6 +88,8 @@ snmp_agent_init(void)
 #if LWIP_SNMP_V3
   snmpv3_dummy_init();
 #endif
+
+  snmp_set_device_enterprise_oid(&device_enterprise_oid);
 
   snmp_set_mibs(mibs, LWIP_ARRAYSIZE(mibs));
   snmp_init();
