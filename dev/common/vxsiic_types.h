1f0784a81017e007c0a6039a7b43f7dfdf1dab7e
Ilia Slepnev <islepnev@gmail.com>
2021-03-05 22:44:27 +0300

    vxsiic: add types

--- dev/common/vxsiic_types.h
+++ dev/common/vxsiic_types.h
@@ -20,6 +20,10 @@
 
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 enum {VXSIIC_SLOTS = 18};
 
 extern uint8_t vxsiic_map_slot_to_number[VXSIIC_SLOTS];
@@ -27,6 +31,7 @@ extern const char *vxsiic_map_slot_to_label[VXSIIC_SLOTS];
 extern const uint32_t BMC_MAGIC;
 
 typedef enum {
+    // read-only
     VXSIIC_REG_MAGIC = 0,
     VXSIIC_REG_BMC_VER = 1,
     VXSIIC_REG_MODULE_ID = 2,
@@ -35,6 +40,41 @@ typedef enum {
     VXSIIC_REG_IIC_ERRORS = 5,
     VXSIIC_REG_UPTIME = 6,
     VXSIIC_REG_MODULE_SERIAL = 7,
+    // writable
+    VXSIIC_REG_TTVXS_BMC_VER = 8,
+    VXSIIC_REG_TTVXS_MODULE_ID = 9,
+    VXSIIC_REG_TTVXS_MODULE_SERIAL = 10,
+    VXSIIC_REG_TTVXS_FPGA_FW_VER = 11,
+    VXSIIC_REG_TTVXS_UPTIME = 12,
+    // 3 unused registers 13..15
 } vxsiic_data_reg;
+
+enum {VXSIIC_SCRATCH_MEM_START_ADDR = VXSIIC_REG_TTVXS_BMC_VER};
+enum {VXSIIC_SCRATCH_MEM_SIZE = 8};
+
+typedef union {
+    struct __attribute__((packed)) {
+        uint16_t minor : 8;
+        uint16_t major : 8;
+        uint16_t patch : 16;
+    } b;
+    uint32_t raw;
+} bmc_ver_t;
+
+typedef bmc_ver_t fw_version_t;
+
+bmc_ver_t make_bmc_ver(uint8_t major, uint8_t minor, uint16_t patch);
+fw_version_t make_fw_version(uint16_t fw_ver, uint16_t fw_rev);
+
+typedef struct vxsiic_ttvxs_info {
+    bmc_ver_t bmc_ver;
+    uint32_t module_id;
+    uint32_t module_serial;
+    fw_version_t fpga_fw_version;
+} vxsiic_ttvxs_info;
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // VXSIIC_TYPES_H
