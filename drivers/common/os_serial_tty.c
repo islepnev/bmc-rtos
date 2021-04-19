e1d52945ca0229ea11fccfec57f676ced4253112
Ilia Slepnev <islepnev@jinr.ru>
2021-04-19 11:14:50 +0300

    duplicate tty I/O (MCB onboard port)

--- drivers/common/os_serial_tty.c
+++ drivers/common/os_serial_tty.c
@@ -29,19 +29,35 @@
 uint32_t tty_uart_errors = 0;
 
 osMessageQDef(message_q_ttyrx, 1, uint32_t);
-osMessageQDef(message_q_ttytx, 1, uint32_t);
+osMessageQDef(message_q_ttytx_1, 1, uint32_t); // tty
+osMessageQDef(message_q_ttytx_2, 1, uint32_t); // debug
 osMessageQId (message_q_ttyrx_id);
-osMessageQId (message_q_ttytx_id);
+osMessageQId (message_q_ttytx_1_id);
+osMessageQId (message_q_ttytx_2_id);
+
+QueueHandle_t get_queue_by_usart(const USART_TypeDef *usart)
+{
+    if (usart == TTY_USART)
+        return message_q_ttytx_1_id;
+#ifdef DEBUG_USART
+    else if (usart == DEBUG_USART)
+        return message_q_ttytx_2_id;
+#endif
+    return 0;
+}
 
 static void USART_TXE_Callback_FromISR(USART_TypeDef *usart)
 {
-    osEvent event = osMessageGet(message_q_ttytx_id, 0);
-    if (osOK != event.status && osEventMessage != event.status) {
-        // should not happen, ignore
+    QueueHandle_t q = get_queue_by_usart(usart);
+    if (q) {
+        osEvent event = osMessageGet(q, 0);
+        if (osOK != event.status && osEventMessage != event.status) {
+            // should not happen, ignore
+        }
+        if (osOK != event.status)
+            return;
     }
-    if (osOK == event.status) {
-        LL_USART_DisableIT_TXE(usart);
-    }
+    LL_USART_DisableIT_TXE(usart);
 }
 
 static void USART_RXNE_Callback_FromISR(USART_TypeDef *usart)
@@ -90,21 +106,35 @@ int __io_getchar (void)
     return (int)event.value.v;
 }
 
-int __io_putchar(int ch)
+int enqueue_char(USART_TypeDef *usart, int ch)
 {
-    if (!message_q_ttytx_id)
+    QueueHandle_t q = get_queue_by_usart(usart);
+
+    if (!q)
         return EIO;
-    if (osOK == osMessagePut(message_q_ttytx_id, ch, osWaitForever)) {
-        LL_USART_EnableIT_TXE(TTY_USART);
-        LL_USART_TransmitData8(TTY_USART, ch);
+    if (osOK == osMessagePut(q, ch, osWaitForever)) {
+        LL_USART_EnableIT_TXE(usart);
+        LL_USART_TransmitData8(usart, ch);
     }
     return 0;
+}
+
+int __io_putchar(int ch)
+{
+    int ret = 0;
+#ifdef DEBUG_USART
+    enqueue_char(DEBUG_USART, ch);
+#endif
+    ret = enqueue_char(TTY_USART, ch);
+
+    return ret;
 }
 
 void initialize_tty_driver(void)
 {
     message_q_ttyrx_id = osMessageCreate(osMessageQ(message_q_ttyrx), NULL);
-    message_q_ttytx_id = osMessageCreate(osMessageQ(message_q_ttytx), NULL);
-    if (!message_q_ttyrx_id || !message_q_ttytx_id)
+    message_q_ttytx_1_id = osMessageCreate(osMessageQ(message_q_ttytx_1), NULL);
+    message_q_ttytx_2_id = osMessageCreate(osMessageQ(message_q_ttytx_2), NULL);
+    if (!message_q_ttyrx_id || !message_q_ttytx_1_id || !message_q_ttytx_2_id)
         Error_Handler();
 }
