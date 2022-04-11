ab50cb9e463379cf4311ebf68574538439339414
Ilia Slepnev <islepnev@jinr.ru>
2022-04-11 16:23:19 +0300

    add TDC64VHLE-v1 board

---
+++ app/tdc64vhlev1/src/bsp/bsp_fpga.h
@@ -0,0 +1,44 @@
+/*
+**    Copyright 2019-2020 Ilja Slepnev
+**
+**    This program is free software: you can redistribute it and/or modify
+**    it under the terms of the GNU General Public License as published by
+**    the Free Software Foundation, either version 3 of the License, or
+**    (at your option) any later version.
+**
+**    This program is distributed in the hope that it will be useful,
+**    but WITHOUT ANY WARRANTY; without even the implied warranty of
+**    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**    GNU General Public License for more details.
+**
+**    You should have received a copy of the GNU General Public License
+**    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef BSP_FPGA_H
+#define BSP_FPGA_H
+
+#include "dev_common_types.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern sensor_list_t fpga_sensor_map;
+
+enum {
+    FPGA_REG_BASE_CSR = 0x0040,
+    FPGA_REG_BASE_TRIG_CTRL = 0x0100,
+    FPGA_REG_BASE_TDC_CORE = 0x0200,
+    FPGA_REG_BASE_WR_EMU = 0x0400,
+    FPGA_REG_BASE_RUNSTAT = 0x0700,
+    FPGA_REG_BASE_STATS = 0x0900,
+    FPGA_REG_BASE_ERRCNT = 0x1000,
+    FPGA_REG_BASE_MCU = 0x4000,
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // BSP_FPGA_H
