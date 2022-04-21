17666ec44e48fb978ed7b1374e3bfc1af9915e6e
Ilia Slepnev <islepnev@jinr.ru>
2022-04-21 16:12:50 +0300

    [task_display] reduce display thread stack

--- src/rtos/app_task_display.c
+++ src/rtos/app_task_display.c
@@ -25,7 +25,7 @@
 
 static const uint32_t displayTaskLoopDelay = 50;
 osThreadId displayThreadId = NULL;
-enum { displayThreadStackSize = threadStackSize + 550 };
+enum { displayThreadStackSize = threadStackSize + 450 };
 
 static void start_display_task(void const *arg)
 {
